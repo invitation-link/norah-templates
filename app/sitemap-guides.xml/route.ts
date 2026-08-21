d244fa653ca39e8d0e24a376fbcc5fd3b2f10e6a
invitation-link <invitationlink1@gmail.com>
2026-08-21 11:45:00 +0530

    feat(seo): add route.ts

---
+++ app/sitemap-guides.xml/route.ts
@@ -0,0 +1,8 @@
+import { publicGuideRoutes } from "@/app/lib/seo-routes";
+import { urlSet, xmlResponse } from "@/app/lib/sitemap-xml";
+
+export const dynamic = "force-static";
+
+export function GET() {
+  return xmlResponse(urlSet(publicGuideRoutes));
+}
